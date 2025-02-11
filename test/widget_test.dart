00ca6736b97ef38520f75204ce83eb0ce1cf7abc
Galuh DAP <90491250+Galuhdap@users.noreply.github.com>
2025-02-11 21:31:48 +0700

    update UnitTest Screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:klinik_web_responsif/presentations/login/screen/login_screen.dart';
 
-import 'package:klinik_web_responsif/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LoginScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
